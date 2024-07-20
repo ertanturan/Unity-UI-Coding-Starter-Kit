af87e6cb25581d424e84613446800379ec1d93fc
Ertan Turan <ertanturan@outlook.com>
2024-07-20 15:21:47 +0100

    syntax fix

--- Assets/CustomTools/UI_Essentials/Components/Base Components/BaseComponent.cs
+++ Assets/CustomTools/UI_Essentials/Components/Base Components/BaseComponent.cs
@@ -5,8 +5,7 @@ namespace CustomTools.UI_Essentials.Components
 {
     public class BaseComponent<T> : MonoBehaviour where T : Selectable
     {
-        public  T Component { get => _component; private set => _component = value; }
-        private T _component;
+        public  T Component { get; private set; }
 
         protected virtual void Awake()
         {
